7aafab5d39f01ad27aa60e598fc8ad77b58a5c05
lavitesse2 <2293518@cegeplimoilou.ca>
2025-05-26 13:05:26 -0400

    projet banque tardi

---
+++ BanqueTardiTP/BanqueTardi/Controllers/AssurancesController.cs
@@ -0,0 +1,88 @@
+﻿using BanqueTardi.Data;
+using BanqueTardi.Models;
+using BanqueTardi.MVC.Interface;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace BanqueTardi.MVC.Controllers
+{
+    public class AssurancesController : Controller
+    {
+        private readonly IAssurancesService _assurancesService;
+        private readonly RedisCacheService _redisCacheService;
+        private readonly ClientContext _context;
+
+        private const string CacheKey = "assurances_liste";
+
+        public AssurancesController(RedisCacheService cacheService, IAssurancesService assurancesService, ClientContext context)
+        {
+            _assurancesService = assurancesService;
+            _redisCacheService = cacheService;
+            _context = context;
+        }
+
+        // GET: AssurancesController
+        public async Task<IActionResult> Index()
+        {
+            // Vérifier si les contrats sont déjà en cache
+            var contrats = await _redisCacheService.GetCacheAsync<List<ContratAssurance>>(CacheKey);
+
+            if (contrats == null)
+            {
+                // Si non, appeler l'API et stocker les données en cache
+                contrats = await _assurancesService.ObtenirTout();
+                if (contrats != null)
+                {
+                    await _redisCacheService.SetCacheAsync(CacheKey, contrats, 12);
+                }
+            }
+
+            return View(contrats);
+        }
+
+        // GET: AssurancesController/Create
+        public IActionResult Create()
+        {
+            PopulateClientsDropDownList();
+            return View();
+        }
+
+        // POST: AssurancesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ContratAssurance contratAssurance)
+        {
+            if (ModelState.IsValid)
+            {
+                Client client = await _context.Clients.FindAsync(contratAssurance.IdClient);
+                contratAssurance.DateNaissance = client.DateNaissance;
+                contratAssurance.NomDemandeur = client.Prenom + " " + client.Nom;
+                contratAssurance.CodePartenaire = "BANQUE";
+
+                await _assurancesService.Ajouter(contratAssurance);
+
+                // Supprimer les anciennes données en cache pour forcer une mise à jour
+                await _redisCacheService.RemoveCacheAsync(CacheKey);
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            PopulateClientsDropDownList();
+            return View(contratAssurance);
+        }
+
+        private void PopulateClientsDropDownList()
+        {
+            ViewData["IdClient"] = new SelectList(
+                _context.Clients.Select(c => new
+                {
+                    Identifiant = c.ID,
+                    PrenomNom = c.Prenom + " " + c.Nom
+                }),
+                "Identifiant",
+                "PrenomNom");
+        }
+    }
+}
---
+++ BanqueTardiTP/BanqueTardi/Migrations/20250331222340_initialCreated.cs
@@ -0,0 +1,521 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanqueTardi.MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TauxInteretDecouvert",
+                table: "TypeComptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TauxInteret",
+                table: "TypeComptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Libelle",
+                table: "TypeComptes",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Identifiant",
+                table: "TypeComptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeOperation",
+                table: "Operations",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeCompteID",
+                table: "Operations",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Montant",
+                table: "Operations",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Libelle",
+                table: "Operations",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOperation",
+                table: "Operations",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompteId",
+                table: "Operations",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OperationId",
+                table: "Operations",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientId",
+                table: "Comptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BanqueId",
+                table: "Comptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeCompteID",
+                table: "Comptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompteId",
+                table: "Comptes",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TelephoneParent",
+                table: "Clients",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Clients",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Prenom",
+                table: "Clients",
+                type: "TEXT",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomParent",
+                table: "Clients",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nom",
+                table: "Clients",
+                type: "TEXT",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NbDecouverts",
+                table: "Clients",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateNaissance",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodePostale",
+                table: "Clients",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Adresse",
+                table: "Clients",
+                type: "TEXT",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Clients",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransitID",
+                table: "Banques",
+                type: "INTEGER",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nom",
+                table: "Banques",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InstitutionID",
+                table: "Banques",
+                type: "INTEGER",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Banques",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TauxInteretDecouvert",
+                table: "TypeComptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TauxInteret",
+                table: "TypeComptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Libelle",
+                table: "TypeComptes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Identifiant",
+                table: "TypeComptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TypeOperation",
+                table: "Operations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeCompteID",
+                table: "Operations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Montant",
+                table: "Operations",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Libelle",
+                table: "Operations",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateOperation",
+                table: "Operations",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompteId",
+                table: "Operations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OperationId",
+                table: "Operations",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClientId",
+                table: "Comptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BanqueId",
+                table: "Comptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TypeCompteID",
+                table: "Comptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CompteId",
+                table: "Comptes",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TelephoneParent",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Prenom",
+                table: "Clients",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NomParent",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nom",
+                table: "Clients",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NbDecouverts",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateNaissance",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CodePostale",
+                table: "Clients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Adresse",
+                table: "Clients",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Clients",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TransitID",
+                table: "Banques",
+                type: "int",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldMaxLength: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nom",
+                table: "Banques",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InstitutionID",
+                table: "Banques",
+                type: "int",
+                maxLength: 3,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldMaxLength: 3);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Banques",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER")
+                .Annotation("Sqlite:Autoincrement", true)
+                .OldAnnotation("Sqlite:Autoincrement", true);
+        }
+    }
+}
